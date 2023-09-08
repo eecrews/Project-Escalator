1089ac9184d47e39485d0429aafebea925b0dee1
eecrews <61025723+eecrews@users.noreply.github.com>
2023-09-08 16:31:21 -0500

    Regex fixes + readme updates

--- Project-Escalator/README.md
+++ Project-Escalator/README.md
@@ -1,2 +1,5 @@
 Built off of https://github.com/slack-samples/bolt-java-starter-template
 
+Known issues:
+- Environment detection is case-sensitive
+- Problems with issue duplication if app is integrated into multiple channels 
--- Project-Escalator/src/main/java/listeners/messages/MessageListeners.java
+++ Project-Escalator/src/main/java/listeners/messages/MessageListeners.java
@@ -7,7 +7,7 @@ import listeners.ListenerProvider;
 public class MessageListeners implements ListenerProvider {
     @Override
     public void register(App app) {
-        Pattern envPattern = Pattern.compile("\\b.*(Zoom|Salesforce|App).*\\b");
+        Pattern envPattern = Pattern.compile(".*(Zoom|Salesforce|App).*");
         app.message(envPattern, new IssueMessageListener(app));
     }
 }
